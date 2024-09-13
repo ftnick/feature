44ec09e848e15d65a8f808218fa8acaf5ffa8a52
nick <153327552+ftnick@users.noreply.github.com>
2024-09-14 09:56:13 +1000

    Upload Website Files
    Changed up files,
    ! PLACEHOLDER !

--- docs/resources/js/script.js
+++ docs/resources/js/script.js
@@ -9,54 +9,65 @@ var getAll = function (selector, scope) {
   return scope.querySelectorAll(selector);
 };
 
-//in page scrolling for documentaiton page
+//in-page scrolling for documentation page
 var btns = getAll('.js-btn');
 var sections = getAll('.js-section');
 
-function setActiveLink(event) {
+function setActiveLink(index) {
   // remove all active tab classes
   for (var i = 0; i < btns.length; i++) {
     btns[i].classList.remove('selected');
   }
 
-  event.target.classList.add('selected');
+  // add active class to the current section
+  btns[index].classList.add('selected');
 }
 
 function smoothScrollTo(i, event) {
   var element = sections[i];
-  setActiveLink(event);
+  setActiveLink(i);
 
   window.scrollTo({
-    'behavior': 'smooth',
-    'top': element.offsetTop - 20,
-    'left': 0
+    behavior: 'smooth',
+    top: element.offsetTop - 20,
+    left: 0
   });
+
+  event.preventDefault();
 }
 
 if (btns.length && sections.length > 0) {
-  for (var i = 0; i<btns.length; i++) {
-    btns[i].addEventListener('click', smoothScrollTo.bind(this,i));
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', smoothScrollTo.bind(this, i));
   }
 }
 
 // fix menu to page-top once user starts scrolling
 window.addEventListener('scroll', function () {
   var docNav = get('.doc__nav > ul');
-
-  if( docNav) {
+  
+  if (docNav) {
     if (window.pageYOffset > 63) {
       docNav.classList.add('fixed');
     } else {
       docNav.classList.remove('fixed');
     }
   }
+
+  // Highlight menu item based on scroll position
+  var currentPosition = window.pageYOffset;
+  sections.forEach(function (section, index) {
+    if (currentPosition >= section.offsetTop - 100 && currentPosition < section.offsetTop + section.offsetHeight) {
+      setActiveLink(index);
+    }
+  });
 });
 
 // responsive navigation
 var topNav = get('.menu');
 var icon = get('.toggle');
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
   function showNav() {
     if (topNav.className === 'menu') {
       topNav.className += ' responsive';
@@ -68,4 +79,3 @@ window.addEventListener('load', function(){
   }
   icon.addEventListener('click', showNav);
 });
-
