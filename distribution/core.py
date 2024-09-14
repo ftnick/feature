231f6217e7a209152d051ffb40ea890745af2bb8
nick <153327552+ftnick@users.noreply.github.com>
2024-09-14 11:12:19 +1000

    scriptupload

--- distribution/core.py
+++ distribution/core.py
@@ -1,11 +1,20 @@
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def version():
     try:
-        response = requests.get("nort")
+        response = requests.get("https://raw.githubusercontent.com/ftnick/feature/main/host/version.txt")
         response.raise_for_status()
         version = response.text.strip()
         return version
-    except requests.RequestException as e:
-        print(f"Error fetching version: {e}")
-        return None
+    except requests.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+    except requests.ConnectionError as conn_err:
+        logging.error(f"Connection error occurred: {conn_err}")
+    except requests.Timeout as timeout_err:
+        logging.error(f"Timeout error occurred: {timeout_err}")
+    except requests.RequestException as req_err:
+        logging.error(f"Error occurred: {req_err}")
+    return None
